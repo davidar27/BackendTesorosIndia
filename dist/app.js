db438172c60101f3376bb5e6c196cf821f255f19
davidar27 <jhonatanarcos10@gmail.com>
2025-03-26 23:34:09 -0500

    Remove unused authentication middleware and models; clean up codebase

--- dist/app.js
+++ dist/app.js
@@ -10,12 +10,15 @@ const authRoutes_1 = __importDefault(require("./routes/Auth/authRoutes"));
 const User_1 = __importDefault(require("./routes/User/User"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 dotenv_1.default.config();
+const cartRoutes_1 = __importDefault(require("./routes/cartRoutes"));
 const app = (0, express_1.default)().use(body_parser_1.default.json());
 app.use((0, cookie_parser_1.default)());
+app.use(express_1.default.json());
 app.use('/user', User_1.default);
 app.use('/admin', User_1.default);
 app.use('/user', authRoutes_1.default);
-const PORT = process.env.PORT || 3000;
+app.use('/cart', cartRoutes_1.default);
+const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`API listening at http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
