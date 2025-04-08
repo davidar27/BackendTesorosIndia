086c58e31cd48b8c12eb4a38dc61f1b721b5b23f
davidar27 <jhonatanarcos10@gmail>
2025-04-08 15:11:11 -0500

    delete logs in azure.ts

--- src/config/azure.ts
+++ src/config/azure.ts
@@ -6,13 +6,5 @@ dotenv.config();
 const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING || "";
 const AZURE_CONTAINER_NAME = process.env.AZURE_CONTAINER_NAME || "";
 
-if (!AZURE_STORAGE_CONNECTION_STRING) {
-    throw new Error("Falta la cadena de conexión de Azure Storage");
-}
-
-if (!AZURE_CONTAINER_NAME) {
-    throw new Error("Falta el nombre del contenedor de Azure Storage");
-}
-
 export const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
 export const containerClient = blobServiceClient.getContainerClient(AZURE_CONTAINER_NAME);
