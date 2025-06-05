94397c8d42654140e52a8d59049459bf8d837e15
davidar27 <jhonatanarcos10@gmail.com>
2025-06-05 16:11:29 -0500

    refactor: update cookie domain settings to use FRONTEND_URL for production environment

--- src/config/cookie.ts
+++ src/config/cookie.ts
@@ -1,4 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
+const domain = process.env.FRONTEND_URL;
 import { CookieOptions } from 'express';
 
 
@@ -8,7 +9,7 @@ export const cookieOptionsLogin: CookieOptions = {
     sameSite: isProduction ? 'none' : 'lax',
     maxAge: 1000 * 60 * 5, // 5 minutos
     path: '/',
-    domain: isProduction ? '.onrender.com' : undefined
+    domain: isProduction ? domain : undefined
 };
 
 export const cookieOptionsRefresh: CookieOptions = {
@@ -17,7 +18,7 @@ export const cookieOptionsRefresh: CookieOptions = {
     sameSite: isProduction ? 'none' : 'lax',
     path: '/',
     maxAge: 1000 * 60 * 60 * 24 * 7, // 7 días
-    domain: isProduction ? '.onrender.com' : undefined
+    domain: isProduction ? domain : undefined
 };
 
 export const cookieOptionsLogout: CookieOptions = {
@@ -25,5 +26,5 @@ export const cookieOptionsLogout: CookieOptions = {
     secure: isProduction,
     sameSite: isProduction ? 'none' : 'lax',
     path: '/',
-    domain: isProduction ? '.onrender.com' : undefined
+    domain: isProduction ? domain : undefined
 };
