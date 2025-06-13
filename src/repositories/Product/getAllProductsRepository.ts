bb1d308cc9db36817abf323aa8befcf018c3e91c
kelvinocampo <kevinocampooso@gmail.com>
2025-06-13 13:11:48 -0500

    feat: get product with category

--- src/repositories/Product/getAllProductsRepository.ts
+++ src/repositories/Product/getAllProductsRepository.ts
@@ -7,15 +7,19 @@ export async function getAllProductsRepository(): Promise<Product[]> {
             s.nombre,
             s.precio,
             s.imagen,
-            COALESCE(ROUND(AVG(v.puntuacion), 2), 0) as puntuacion
+            COALESCE(ROUND(AVG(v.puntuacion), 2), 0) as puntuacion,
+            GROUP_CONCAT(DISTINCT c.nombre SEPARATOR ';') AS categoria
         FROM servicio s
         LEFT JOIN experiencia e ON s.experiencia_id = e.experiencia_id
         LEFT JOIN valoracion v ON e.experiencia_id = v.experiencia_id
+        LEFT JOIN servicio_categoria sc ON sc.servicio_id = s.servicio_id
+        LEFT JOIN categoria c ON c.categoria_id = sc.categoria_id
         WHERE s.estado = 'activo' AND s.tipo = 'producto'
         GROUP BY 
             s.nombre, 
             s.precio, 
-            s.imagen
+            s.imagen,
+            c.nombre
         ORDER BY s.nombre ASC;
     `;
     const [rows]: any = await db.execute(sql);
