a4ca5054a4685d7dcf754934128ca0712c115758
TesorosIndia <tesorosindia692@gmail.com>
2025-06-18 13:20:47 -0500

    refactor: get experience members

--- src/repositories/Experience/getExperienceMembersRepository.ts
+++ src/repositories/Experience/getExperienceMembersRepository.ts
@@ -3,13 +3,13 @@ import db from '@/config/db';
 export const getExperienceMembersRepository = async (experience_id: number) => {
     const sql = `
         SELECT
-            u.nombre AS name,
-            u.profesion AS profession,
-            u.edad AS age,
-            e.descripcion AS description
-        FROM usuario u
-        JOIN experiencia e ON u.experiencia_id = e.experiencia_id
-        WHERE u.rol = 'emprendedor' AND u.estado = 'activo' AND e.experiencia_id = ?;
+            i.nombre AS name,
+            i.profesion AS profession,
+            i.edad AS age,
+            i.descripcion AS description
+        FROM integrantes i
+        JOIN experiencia e ON i.experiencia_id = e.experiencia_id
+        WHERE e.experiencia_id = ?;
     `;
     const [rows]: any = await db.execute(sql, experience_id);
     return rows;
