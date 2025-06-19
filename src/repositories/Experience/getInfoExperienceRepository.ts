b02c2a6a98a8b9e5bd28354fcf7d84ab3ff87112
davidar27 <jhonatanarcos10@gmail.com>
2025-06-19 14:17:48 -0500

    feat: enhance experience info retrieval with additional fields

--- src/repositories/Experience/getInfoExperienceRepository.ts
+++ src/repositories/Experience/getInfoExperienceRepository.ts
@@ -3,10 +3,14 @@ import db from '@/config/db';
 export const getInfoExperienceRepository = async (id_experience: number) => {
     const sql = `
         SELECT
+            e.experiencia_id as id,
             e.nombre AS name,
             e.tipo AS type,
             e.descripcion AS description,
-            e.historia AS history
+            e.historia AS history,
+            e.imagen as image,
+            e.latitud as lat,
+            e.longitud as lng
         FROM experiencia e
         WHERE e.experiencia_id = ?;
     `;
