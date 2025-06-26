e1da3db5bcdd31fc65d636dbec47f9c519e6ae47
TesorosIndia <tesorosindia692@gmail.com>
2025-06-26 17:46:07 -0500

    feat: get info package

--- src/routes/Package/packageRoutes.ts
+++ src/routes/Package/packageRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { getPacksController } from "@/controllers/Package/getPackagesController";
 import { searchPackagesController } from "@/controllers/Package/searchPackagesController";
+import { getPackageController } from "@/controllers/Package/getPackController";
 const router = express.Router();
 
 router.get("/buscar", searchPackagesController);
 router.get("/", getPacksController);
+router.get("/:package_id", getPackageController);
 
 export default router;
