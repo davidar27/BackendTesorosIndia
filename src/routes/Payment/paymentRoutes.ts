3734a68ed2263be7fa2c48e70e727971e3d74271
davidar27 <jhonatanarcos10@gmail.com>
2025-06-26 10:20:11 -0500

    ...

--- src/routes/Payment/paymentRoutes.ts
+++ src/routes/Payment/paymentRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { webhookController } from '../../controllers/Payment/webhookController';
-import { handlerPreference } from '@/controllers/Payment/Preference'
+// import { webhookController } from '../../controllers/Payment/webhookController';
+import { handlerPreference } from '@/controllers/Payment/handlerPreference'
 
 const router = Router();
 router.post('/preferencia', handlerPreference);
