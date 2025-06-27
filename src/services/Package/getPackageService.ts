6767d39416c8677d9756aac7e862afafbc23e18a
TesorosIndia <tesorosindia692@gmail.com>
2025-06-27 13:00:20 -0500

    ....

--- src/services/Package/getPackageService.ts
+++ src/services/Package/getPackageService.ts
@@ -6,6 +6,6 @@ export const getPackageService = async (package_id: number) => {
     const info = await getPackageRepository(package_id);
     const details = await getPackageDetailsRepository(package_id);
     const experiences = await getPackageExperiencesRepository(package_id);
-    const pack = { info: info, experiences: experiences, details: details }
+    const pack = { ...info, experiences: experiences, details: details }
     return pack
 }; 
