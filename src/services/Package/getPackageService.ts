bb16df180025b94b8430d34eee30c59395c27c88
TesorosIndia <tesorosindia692@gmail.com>
2025-06-27 13:01:12 -0500

    ...

--- src/services/Package/getPackageService.ts
+++ src/services/Package/getPackageService.ts
@@ -3,7 +3,7 @@ import { getPackageExperiencesRepository } from "@/repositories/Package/getPacka
 import { getPackageRepository } from "@/repositories/Package/getPackageRepository";
 
 export const getPackageService = async (package_id: number) => {
-    const info = await getPackageRepository(package_id);
+    const [info] = await getPackageRepository(package_id);
     const details = await getPackageDetailsRepository(package_id);
     const experiences = await getPackageExperiencesRepository(package_id);
     const pack = { ...info, experiences: experiences, details: details }
