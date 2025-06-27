ed7c355abccc8a2f1c17b7163b20a31d4c2daca0
TesorosIndia <tesorosindia692@gmail.com>
2025-06-27 12:59:07 -0500

    ...

--- src/repositories/Package/getPackageRepository.ts
+++ src/repositories/Package/getPackageRepository.ts
@@ -3,11 +3,14 @@ import db from '@/config/db';
 export const getPackageRepository = async (package_id: number) => {
     const sql = `
         SELECT
-            d.detalle_id AS detail_id,
-            d.descripcion AS detail
-        FROM detalle d
-        JOIN servicio_detalle sd ON d.detalle_id = sd.detalle_id
-        JOIN servicio s ON sd.servicio_id = s.servicio_id
+            s.imagen AS image,
+            CONCAT( FORMAT(s.precio, 0, "es_CO")) as price,
+            s.nombre AS name,
+            s.servicio_id AS package_id,
+            s.descripcion AS description,
+            s.capacidad AS capacity,
+            s.duracion AS duration
+        FROM servicio s
         WHERE s.tipo = 'paquete' AND s.estado = 'activo' AND s.servicio_id = ?
     `;
     const [rows]: any = await db.execute(sql, [package_id]);
