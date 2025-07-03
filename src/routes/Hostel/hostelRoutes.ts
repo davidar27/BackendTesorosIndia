c6fcfab4a2d16190d2b9dbca593ff3b1fd5afb89
davidar27 <jhonatanarcos10@gmail.com>
2025-07-03 17:02:24 -0500

    refactor: rename parameters for consistency and enhance SQL query to include average rating for hostels

--- src/routes/Hostel/hostelRoutes.ts
+++ src/routes/Hostel/hostelRoutes.ts
@@ -1,12 +1,10 @@
 import { getHostelsController } from "@/controllers/Hostel/getHostelsController";
 import { getRoomsByHostelController } from "@/controllers/Hostel/getRoomsByHostelController";
-import { authMiddlewareToken } from "@/middleware/Auth/authMiddlewareToken";
-import { checkRole } from "@/middleware/Auth/checkRole";
 import express from "express";
 
 const router = express.Router();
 
 router.get("/", getHostelsController);
-router.get("/:hostel_id", getRoomsByHostelController);
+router.get("/:hostel_id/rooms", getRoomsByHostelController);
 
 export default router;
