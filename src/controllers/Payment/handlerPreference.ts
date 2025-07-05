f17ada40ce8717c39c65183a1f614017984d7737
davidar27 <jhonatanarcos10@gmail.com>
2025-07-05 12:19:30 -0500

    feat: enhance payment preference handler to include payer details and improve error handling

--- src/controllers/Payment/handlerPreference.ts
+++ src/controllers/Payment/handlerPreference.ts
@@ -1,25 +1,35 @@
-import {  Preference } from 'mercadopago';
+import { Preference } from 'mercadopago';
 import { Request, Response } from 'express';
 import { mercadopagoClient } from '@/config/mercadopago';
 
 const FRONTEND_URL = process.env.FRONTEND_URL;
-    
 
 export const handlerPreference = async (req: Request, res: Response) => {
     try {
         const data = req.body;
-        if (!data) {
-            return res.status(400).json({ error: 'No se proporcionaron datos' });
+        if (!data || !data.items) {
+            return res.status(400).json({ error: 'Datos incompletos' });
         }
+
         const preference = new Preference(mercadopagoClient);
         const response = await preference.create({
             body: {
-                items: data.items.map((item: any) => ({                    
+                items: data.items.map((item: any) => ({
                     id: item.id,
                     title: item.title,
                     unit_price: item.unit_price,
                     quantity: item.quantity,
                 })),
+                payer: {
+                    name: data.payer.name,
+                    surname: data.payer.surname,
+                    email: data.payer.email,
+                    address: {
+                        street_name: data.payer.address.street_name,
+                        street_number: data.payer.address.street_number,
+                        zip_code: data.payer.address.zip_code,
+                    }
+                },
                 back_urls: {
                     success: `${FRONTEND_URL}/pago-exitoso`,
                     failure: `${FRONTEND_URL}/pago-fallido`,
@@ -30,8 +40,9 @@ export const handlerPreference = async (req: Request, res: Response) => {
                     installments: 1,
                     default_installments: 1,
                 },
-            }            
+            }
         });
+
         return res.status(200).json({
             preferenceId: response.id,
             init_point: response.init_point,
@@ -49,5 +60,4 @@ export const handlerPreference = async (req: Request, res: Response) => {
             status: 'preference_creation_failed',
         });
     }
-}
-
+};
