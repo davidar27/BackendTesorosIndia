b948e16d684b391d5b5fbfde7ed85f80a5d4d165
davidar27 <jhonatanarcos10@gmail.com>
2025-07-05 21:24:02 -0500

    fix: update SQL insert statement to use reserva_habitacion and include user_id, add console logs for debugging in reserveController

--- src/repositories/Reserve/reserveRepository.ts
+++ src/repositories/Reserve/reserveRepository.ts
@@ -3,9 +3,9 @@ import { Reserve } from '@/models/Reserve/Reserve';
 
 export const reserveRepository = async (reserve: Reserve) => {
     const sql = `
-        INSERT INTO reserva_servicio
+        INSERT INTO reserva_habitacion
         (fecha_reserva, estado, habitacion_id, usuario_id)
-        VALUES(?, ?, ?)
+        VALUES(?, ?, ?, ?)
     `;
     const values = [reserve.reserve_date, reserve.state, reserve.room_id, reserve.user_id]
     const [rows]: any = await db.execute(sql, values);
