0c146897a46b03be4663918217d9d6f5fab19559
TesorosIndia <Tesorosindia692@gmail.com>
2025-07-06 10:19:28 -0500

    ...

--- src/repositories/payment/cancelBuyRepository.ts
+++ src/repositories/payment/cancelBuyRepository.ts
@@ -2,7 +2,9 @@ import db from '@/config/db';
 
 export const cancelBuyRepository = async (bill_id: number, user_id: number) => {
     const sql = `
-
+        UPDATE factura
+        SET estado = 'cancelada'
+        WHERE factura_id = ? AND usuario_id = ?
     `;
     const [rows]: any = await db.execute(sql, [bill_id, user_id]);
     return rows;
