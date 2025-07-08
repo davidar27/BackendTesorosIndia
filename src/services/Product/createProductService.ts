a1105fa9326ddd7bb67c06530c11f2af2b70ebb4
davidar27 <jhonatanarcos10@gmail.com>
2025-07-08 11:31:54 -0500

    feat: update product creation logic to return product details and enhance SQL queries for consistency

--- src/services/Product/createProductService.ts
+++ src/services/Product/createProductService.ts
@@ -1,7 +1,7 @@
 import { Product } from "@/models/Product/Product";
 import { createProductRepository } from "@/repositories/Product/createProductRepository";
 
-export async function createProductService(product: Product): Promise<boolean> {
-    const result = await createProductRepository(product);
-    return result ? true : false
+export async function createProductService(product: Product): Promise<Product> {
+    const insertId = await createProductRepository(product);
+    return { ...product, product_id: insertId };
 }
