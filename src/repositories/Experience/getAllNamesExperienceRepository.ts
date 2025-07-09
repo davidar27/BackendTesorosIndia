3a7384ad3fecb092b64bad982f00dbebe3f15366
davidar27 <jhonatanarcos10@gmail.com>
2025-07-08 20:25:24 -0500

    feat: enhance category and experience repositories with additional fields and improved SQL queries

--- src/repositories/Experience/getAllNamesExperienceRepository.ts
+++ src/repositories/Experience/getAllNamesExperienceRepository.ts
@@ -3,7 +3,14 @@ import { Experience } from '@/models/Experience/Experience';
 
 
 export const getAllNamesExperienceRepository = async (): Promise<Experience> => {
-    const sql = `SELECT experiencia_id AS id, nombre AS name_experience, tipo as type FROM experiencia WHERE estado = 'publicada'`;
+    const sql = `SELECT 
+    experiencia_id AS id,
+    nombre AS name_experience,
+    tipo as type, 
+    imagen as image, 
+    descripcion as description 
+    FROM experiencia 
+    WHERE estado = 'publicada'`;
     const [rows]: any = await db.execute(sql);
     return rows;
 };
